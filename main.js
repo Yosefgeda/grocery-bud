bf8c21e3bb8aa0ea5cbde590905cfb2c84b5cacc
Yosefgeda <100337086+Yosefgeda@users.noreply.github.com>
2023-10-31 18:31:53 +0300

    Functionality for empty input

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const alert = document.querySelector('.alert');
+const alertContainer = document.querySelector('.alert');
 const form = document.querySelector('.grocery-form');
 const grocery = document.getElementById('grocery');
 const submitBtn = document.querySelector('.submit-btn');
@@ -10,9 +10,31 @@ let editElement;
 let editFlag = false;
 let editId = "";
 
-form.addEventListener('submit', addItem)
+
 
 const addItem = (e) => {
     e.preventDefault();
+
+    const value = grocery.value;
+    
+    if( value && !editFlag ){
+
+    } else if( value && editFlag) {
+
+    } else {
+        displaAlert('please enter an item', 'alert-danger')
+    }
     
 }
+
+const displaAlert = (alertMsg, action) => {
+    alertContainer.textContent = alertMsg;
+    alert.classList = action;
+
+    setTimeout(() => {
+        alertContainer.textContent = '';
+        alert.classList.add(action);
+    }, 1000);
+}
+
+form.addEventListener('submit', addItem);
